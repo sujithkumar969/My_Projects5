437121946c00af0ba9c198156775a1b39a091c64
sujith <sujithkumar969@gmail.com>
2020-09-27 12:41:19 +0530

    my commit

--- API_Basics/src/com/qa/googlemap/DeletePlace.java
+++ API_Basics/src/com/qa/googlemap/DeletePlace.java
@@ -1,24 +1,34 @@
-package com.qa.test;
+package com.qa.googlemap;
 
 import org.testng.annotations.Test;
 
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
-public class GetPlace {
+public class DeletePlace {
 
 	@Test
-	public void getPlaceTest() {
+	public void deletePlaceTest() {
 		
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		RequestSpecification reqSpec = new RequestSpecBuilder().setContentType(ContentType.JSON)
-				.addQueryParam("key", "qaclick123").build();
+				.build();
 		ResponseSpecification respSpec = new ResponseSpecBuilder().expectContentType(ContentType.JSON)
 				.expectStatusCode(200)
 				.build();
-	}
+		
+		String response = given().spec(reqSpec).body("{     \"place_id\":\"f7eb728c591b74b2252aa18ee6cf417f\" }")
+		.when().delete("maps/api/place/delete/json")
+		.then().assertThat().extract().response().asString();
+		
+		System.out.println(response);
 
+	}
 
 }
