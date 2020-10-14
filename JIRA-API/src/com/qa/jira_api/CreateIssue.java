7afef37d3b39be781b3e2f006c18c44955352c8e
sujith <sujithkumar969@gmail.com>
2020-10-14 14:42:35 +0530

    msg

--- JIRA-API/src/com/qa/jira_api/CreateIssue.java
+++ JIRA-API/src/com/qa/jira_api/CreateIssue.java
@@ -4,8 +4,10 @@ import static io.restassured.RestAssured.given;
 
 import java.io.File;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import io.cucumber.java.en.Given;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
@@ -72,24 +74,80 @@ System.out.println("************************************************************
         System.out.println("-------------------------------");
 		System.out.println(response3);
 		JsonPath jsp3 = new JsonPath(response3);
-		String commentId = jsp3.getString("id");
+		int commentId = jsp3.getInt("id");
+		String comment = jsp3.getString("body");
 		System.out.println("Comment id is :- "+commentId);
 		System.out.println("-------------------------------");
 System.out.println("***********************************************************************************************");
 
 
 // adds attachment to an existing issue using multipart(), header("Content-Type","multipart/form-data") //
-        RequestSpecification reqs4 = given().filter(session).header("X-Atlassian-Token","no-check").pathParam("key", issueId).header("Content-Type","multipart/form-data").
-        		multiPart("file",new File("jira-bug-attachments/exceptions.docx"));
+        RequestSpecification reqs4 = given().spec(reqs).header("X-Atlassian-Token","no-check").pathParam("key", issueId)
+        		                     .header("Content-Type","multipart/form-data")
+        		                 //    .multiPart("file",new File("jira-bug-attachments/Untitled3.png"))
+        		                     .multiPart("file", new File("jira-bug-attachments/exceptions.docx"));
         String response4 = reqs4.when().post("rest/api/2/issue/{key}/attachments")
-                           .then().spec(resp).assertThat().statusCode(200).extract().response().asString();
+                           .then().assertThat().statusCode(200).extract().response().asString(); // if we declare response as json, error will show //
         System.out.println("-------------------------------");
 		System.out.println(response4);
+		JsonPath jsp4 = new JsonPath(response4);
+		String attachmentId = jsp4.getString("id");
+		System.out.println("Attachment id is :- "+attachmentId);
+		System.out.println("-------------------------------");
+System.out.println("***********************************************************************************************");
+
+
+//Get issue details and verify if added comment and attachment exist or not //
+        RequestSpecification reqs5 = given().spec(reqs).pathParam("key", issueId).queryParam("fields", "comment");
+        String response5 = reqs5.when().get("rest/api/2/issue/{key}")
+                           .then().extract().response().asString();
+        System.out.println("-------------------------------");
+		System.out.println(response5);
+		JsonPath jsp5 = new JsonPath(response5);
+		int commentsSize = jsp5.getInt("fields.comment.comments.size()");
+			
+		for(int i=0; i<commentsSize; i++) {	
+			int actualCommentId = jsp5.getInt("fields.comment.comments["+i+"].id");
+			
+			if(commentId == actualCommentId) {
+				
+				String actualComment = jsp5.getString("fields.comment.comments["+i+"].body");
+				Assert.assertEquals(actualComment, comment);
+				System.out.println("Excepted comment - "+comment);
+				System.out.println("actual comment - "+actualComment);
+				break;
+			}  
+		}	
+		System.out.println("-------------------------------");
+System.out.println("***********************************************************************************************");
+
+        // verify added attachment exist or not //
+        RequestSpecification reqs6 = given().spec(reqs).pathParam("key", issueId).queryParam("fields", "attachment");
+        String response6 = reqs6.when().get("rest/api/2/issue/{key}")
+        		          .then().extract().response().asString();
+        System.out.println("-------------------------------");
+		System.out.println(response6);
+		JsonPath jsp6 = new JsonPath(response6);
+		int attachmentSize = jsp6.getInt("fields.attachment.size()");
+		
+		for(int j=0; j<attachmentSize; j++) {	
+			String actualAttachmentId = jsp6.getString("fields.attachment["+j+"].id");
+			
+			System.out.println(attachmentId);
+			System.out.println(actualAttachmentId);
+			
+			if((actualAttachmentId.equals(attachmentId))) {
+				
+				String actualAttachmentName = jsp6.getString("fields.attachment["+j+"].filename");
+				Assert.assertEquals(actualAttachmentName, "exceptions.docx");
+				System.out.println("Excepted attachemnt name - "+"exceptions.docx");
+				System.out.println("Actual attachemnt name - "+actualAttachmentName);
+				break;
+			}  
+		}	
 		System.out.println("-------------------------------");
 System.out.println("***********************************************************************************************");
 
 	}
-
-
-
+	
 }
